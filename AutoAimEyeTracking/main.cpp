24d52657b9c9e0022e7fbcd4d65188fbacae0cba
juanjovn <juanjovn@gmail.com>
2018-04-30 12:45:04 +0100

    cambiando ajustes y añandiendo loading cascades

--- AutoAimEyeTracking/main.cpp
+++ AutoAimEyeTracking/main.cpp
@@ -1,6 +1,21 @@
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
 #include <iostream>
 
+//GLOBAL VARIABLES
+cv::String cascadePath = "./cascades/haarcascade_eye.xml";
+cv::CascadeClassifier cascade;
+cv::String windowTitle = "Auto Aim Eye Tracking";
+
 int main(int argc, const char * argv[]) {
-    std::cout << "Hello, papito\n";
+    
+    if (!cascade.load(cascadePath))
+    {
+        std::cout << "Error loading cascade\n";
+        return -1;
+    }
+    
     return 0;
 }
