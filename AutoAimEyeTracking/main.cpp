e113e1ec6c9152d202106ec25b365aac743a5733
juanjovn <juanjovn@gmail.com>
2018-04-30 11:04:52 +0100

    Initial Commit

---
+++ AutoAimEyeTracking/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AutoAimEyeTracking
+//
+//  Created by Juanjo Valiño on 30/04/2018.
+//  Copyright © 2018 Juanjo Valiño. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
