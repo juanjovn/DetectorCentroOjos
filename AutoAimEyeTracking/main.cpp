f5a0c9c1fa1e201f2e5b98658771d39bd06d8d1c
juanjovn <juanjovn@gmail.com>
2018-05-09 10:28:04 +0100

    Resized the frame, gray and detecting one eye

--- AutoAimEyeTracking/main.cpp
+++ AutoAimEyeTracking/main.cpp
@@ -8,10 +8,15 @@
 cv::String cascadePath = "./cascades/haarcascade_eye.xml";
 cv::CascadeClassifier cascade;
 cv::String windowTitle = "Auto Aim Eye Tracking";
+int camNum = 0;
 
 int main(int argc, const char * argv[]) {
     
     cascade.load(cascadePath);
+    cv::Mat frame;
+    cv::Mat cropped;
+    cv::Mat gray;
+    std::vector<cv::Rect> eyes;
     if (cascade.empty())
     {
         std::cout << "Error loading cascade\n";
@@ -19,7 +24,44 @@ int main(int argc, const char * argv[]) {
     }
     
     cv::namedWindow (windowTitle, CV_WINDOW_NORMAL);
-    cv::VideoCapture camera;
+    cv::VideoCapture camera(0);
+    if (!camera.isOpened())
+    {
+        camera.open(1);
+        camNum=1;
+        if (!camera.isOpened())
+        {
+            camera.open(2);
+            camNum=2;
+            if (!camera.isOpened())
+            {
+                std::cout << "*** UNABLE TO OPEN ANY CAM ***\n*** Press ENTER to exit ***\n";
+                std::cin.get();
+                return -1;
+            }
+        }
+        
+    }
+    
+    while (true){
+        camera.read(frame);
+        cv::resize(frame, cropped, cv::Size(1024, 576));
+        cv::cvtColor(cropped, gray, CV_RGB2GRAY);
+        cascade.detectMultiScale(gray, eyes, 1.1, 2, 0 |cv::CASCADE_SCALE_IMAGE, cv::Size(30, 30));
+        if (eyes.size()> 0)
+        {
+        //std::cout << eyes[0].x << std::endl;
+        cv::rectangle(gray, eyes[0], cv::Scalar(0,255,0), 2);
+        }
+        imshow(windowTitle, gray);
+        if (cv::waitKey(1) >= 0)
+            break;
+    }
+    
+    
+    std::cout << "*** END OF PROGRAM ***\n*** Press ENTER to exit ***\n";
+    std::cin.get();
+    
     
     
     return 0;
