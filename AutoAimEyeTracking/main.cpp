2c50c878cf8154988965c5115f41a16995c54be1
juanjovn <juanjovn@gmail.com>
2018-05-11 16:19:19 +0100

    Added X and Y axis and draw X coordinate at the bottom of the image in red font.

--- AutoAimEyeTracking/main.cpp
+++ AutoAimEyeTracking/main.cpp
@@ -3,6 +3,7 @@
 #include "opencv2/imgproc/imgproc.hpp"
 
 #include <iostream>
+#include <cmath>
 
 #include "findEyeCenter.h"
 
@@ -10,7 +11,7 @@
 cv::String cascadePath = "./cascades/haarcascade_eye.xml";
 cv::CascadeClassifier cascade;
 cv::String windowTitle = "Auto Aim Eye Tracking";
-cv::Rect cropArea(477, 150, 270, 270);
+cv::Rect cropArea(477, 150, 200, 300);
 int camNum = 0;
 
 int main(int argc, const char * argv[]) {
@@ -22,6 +23,8 @@ int main(int argc, const char * argv[]) {
     cv::Mat blurred;
     cv::Mat zoomed;
     cv::Point drawPoint, centerPoint;
+    int xCoord, yCoord = 0;
+    std::string coords;
     std::vector<cv::Rect> eyes;
     if (cascade.empty())
     {
@@ -54,6 +57,8 @@ int main(int argc, const char * argv[]) {
         cv::resize(frame, cropped, cv::Size(1024, 576));
         cropped = cropped(cropArea);
         cv::cvtColor(cropped, gray, CV_RGB2GRAY);
+        cv::arrowedLine(cropped, cv::Point(100,300), cv::Point(100,0), cv::Scalar(0, 255, 255));
+        cv::arrowedLine(cropped, cv::Point(0,150), cv::Point(200,150), cv::Scalar(0, 255, 255));
         cv::GaussianBlur(gray, blurred, cv::Size(11, 25), 0, 0);
         cascade.detectMultiScale(blurred, eyes, 1.1, 1, 0 |cv::CASCADE_SCALE_IMAGE, cv::Size(75, 100));
         if (eyes.size()> 0)
@@ -64,6 +69,10 @@ int main(int argc, const char * argv[]) {
         drawPoint.x = centerPoint.x + eyes[0].x;
         drawPoint.y = centerPoint.y + eyes[0].y;
         cv::circle(cropped, drawPoint, 4, cv::Scalar(255, 0, 255), 2);
+        xCoord = abs(drawPoint.x) - 100;
+        yCoord = abs(drawPoint.y) - 150;
+        coords = std::to_string(xCoord);
+        cv::putText(cropped, coords, cv::Point(0, 290), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 255),2);
         //cv::rectangle(gray, eyes[1], cv::Scalar(0,255,0), 2);
         }
         //cv::resize(cropped, zoomed, cv::Size(540, 540));
